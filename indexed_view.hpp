496eb0a22c9ed0c6f5d2c972239504eea64447cb
Anthony Williams <anthony@justsoftwaresolutions.co.uk>
2019-03-21 15:04:05 +0000

    Dereferencing iterators

--- indexed_view.hpp
+++ indexed_view.hpp
@@ -5,22 +5,47 @@
 namespace jss {
     template <typename UnderlyingIterator> class indexed_view_type {
     public:
-        indexed_view_type(UnderlyingIterator begin_, UnderlyingIterator end_) {}
+        indexed_view_type(
+            UnderlyingIterator &&begin_, UnderlyingIterator &&end_) :
+            source_begin(std::move(begin_)),
+            source_end(std::move(end_)) {}
+
+        struct value_type {
+            size_t index;
+            decltype(*std::declval<UnderlyingIterator>()) &value;
+        };
 
         class iterator {
         public:
             friend constexpr bool
             operator==(iterator const &lhs, iterator const &rhs) noexcept {
-                return true;
+                return lhs.source_iter == rhs.source_iter;
             }
+
+            value_type operator*() const {
+                return value_type{index, *source_iter};
+            }
+
+        private:
+            friend class indexed_view_type;
+
+            iterator(size_t index_, UnderlyingIterator source_iter_) :
+                index(index_), source_iter(std::move(source_iter_)) {}
+
+            size_t index;
+            UnderlyingIterator source_iter;
         };
 
         iterator begin() {
-            return iterator();
+            return iterator(0, source_begin);
         }
         iterator end() {
-            return iterator();
+            return iterator(0, source_end);
         }
+
+    private:
+        UnderlyingIterator source_begin;
+        UnderlyingIterator source_end;
     };
 
     template <typename Range>
--- test_indexed_view.cpp
+++ test_indexed_view.cpp
@@ -1,14 +1,48 @@
 #include "indexed_view.hpp"
 #include <assert.h>
 #include <vector>
+#include <type_traits>
 
 void test_indexed_view_is_empty_for_empty_vector() {
     std::vector<int> v;
-
     auto view= jss::indexed_view(v);
 
     assert(view.begin() == view.end());
 }
+
+void test_indexed_view_iterator_has_index_and_value_of_source() {
+    std::vector<int> v;
+    auto view= jss::indexed_view(v);
+
+    static_assert(
+        std::is_same<
+            typename decltype(view)::iterator, decltype(view.begin())>::value,
+        "begin must return an iterator");
+    static_assert(
+        std::is_same<decltype((*view.begin()).index), size_t>::value,
+        "index is size_t");
+    static_assert(
+        std::is_same<decltype((*view.begin()).value), int &>::value,
+        "value is ref");
+}
+
+void test_dereferencing_begin_iterator_of_indexed_view_gives_0_and_element() {
+    std::vector<int> v{42, 56, 99};
+    auto view= jss::indexed_view(v);
+
+    assert((*view.begin()).index == 0);
+    assert(&(*view.begin()).value == &v[0]);
+}
+
+void test_begin_and_end_of_non_empty_range_are_not_equal() {
+    std::vector<int> v{42, 56, 99};
+    auto view= jss::indexed_view(v);
+    assert(!(view.begin() == view.end()));
+}
+
 int main() {
     test_indexed_view_is_empty_for_empty_vector();
+    test_indexed_view_iterator_has_index_and_value_of_source();
+    test_dereferencing_begin_iterator_of_indexed_view_gives_0_and_element();
+    test_begin_and_end_of_non_empty_range_are_not_equal();
 }
