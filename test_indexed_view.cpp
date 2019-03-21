e5d29c9a149e96a4536fce5886f21226f39236d1
Anthony Williams <anthony@justsoftwaresolutions.co.uk>
2019-03-21 16:15:46 +0000

    Verify indexed_view works with input iterator ranges

--- test_indexed_view.cpp
+++ test_indexed_view.cpp
@@ -153,6 +153,151 @@ void test_can_write_through_value_in_range_for() {
     }
 }
 
+template <typename T> struct IncrementValue {
+    void operator()(T &x) const {
+        ++x;
+    }
+};
+
+template <typename T> struct IncrementBy {
+
+    T delta;
+
+    IncrementBy(T delta_) : delta(std::move(delta_)) {}
+
+    void operator()(T &x) const {
+        x+= delta;
+    }
+};
+
+template <typename T, typename Increment= IncrementValue<T>>
+class numeric_range {
+public:
+    enum class direction { increasing, decreasing };
+
+private:
+    T current;
+    T final;
+    Increment inc;
+    direction dir;
+
+    bool at_end() {
+        if(dir == direction::increasing) {
+            return current >= final;
+        } else {
+            return current <= final;
+        }
+    }
+
+public:
+    class iterator {
+        numeric_range *range;
+
+        void check_done() {
+            if(range->at_end()) {
+                range= nullptr;
+            }
+        }
+
+        class postinc_return {
+            T value;
+
+        public:
+            postinc_return(T value_) : value(std::move(value_)) {}
+            T operator*() {
+                return std::move(value);
+            }
+        };
+
+    public:
+        using value_type= T;
+        using reference= T;
+        using iterator_category= std::input_iterator_tag;
+        using pointer= T *;
+        using difference_type= void;
+
+        iterator(numeric_range *range_) : range(range_) {
+            if(range)
+                check_done();
+        }
+
+        T operator*() const {
+            return range->current;
+        }
+
+        T *operator->() const {
+            return &range->current;
+        }
+
+        iterator &operator++() {
+            if(!range)
+                throw std::runtime_error("Increment a past-the-end iterator");
+            range->inc(range->current);
+            check_done();
+            return *this;
+        }
+
+        postinc_return operator++(int) {
+            postinc_return temp(**this);
+            ++*this;
+            return temp;
+        }
+
+        friend bool operator==(iterator const &lhs, iterator const &rhs) {
+            return lhs.range == rhs.range;
+        }
+        friend bool operator!=(iterator const &lhs, iterator const &rhs) {
+            return !(lhs == rhs);
+        }
+    };
+
+    iterator begin() {
+        return iterator(this);
+    }
+
+    iterator end() {
+        return iterator(nullptr);
+    }
+
+    numeric_range(T initial_, T final_) :
+        current(std::move(initial_)), final(std::move(final_)),
+        dir(direction::increasing) {}
+    numeric_range(T initial_, T final_, Increment inc_) :
+        current(std::move(initial_)), final(std::move(final_)),
+        inc(std::move(inc_)), dir(direction::increasing) {}
+    numeric_range(T initial_, T final_, Increment inc_, direction dir_) :
+        current(std::move(initial_)), final(std::move(final_)),
+        inc(std::move(inc_)), dir(dir_) {}
+};
+
+template <typename T> numeric_range<T> range(T from, T to) {
+    if(to < from)
+        throw std::runtime_error("Cannot count down ");
+    return numeric_range<T>(std::move(from), std::move(to));
+}
+
+void test_can_index_input_ranges() {
+    std::vector<std::pair<size_t, unsigned>> output;
+
+    unsigned const base= 5;
+    unsigned const count= 20;
+
+    for(auto const &x : jss::indexed_view(range(base, base + count))) {
+        output.push_back({x.index, x.value});
+    }
+
+    assert(
+        !std::is_reference<decltype(jss::indexed_view(range(base, base + count))
+                                        .begin()
+                                        ->value)>::value);
+
+    assert(output.size() == count);
+    for(unsigned i= 0; i < output.size(); ++i) {
+        assert(output[i].first == i);
+        assert(output[i].second == (base + i));
+    }
+}
+
 int main() {
     test_indexed_view_is_empty_for_empty_vector();
     test_indexed_view_iterator_has_index_and_value_of_source();
@@ -165,4 +310,5 @@ int main() {
     test_view_iterator_equality_comparisons();
     test_view_iterator_with_range_for();
     test_can_write_through_value_in_range_for();
+    test_can_index_input_ranges();
 }
